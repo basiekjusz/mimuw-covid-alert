4ff6388e5507a481d2875e3be436a03a9c8e0282
basiekjusz <jasiekbusz@gmail.com>
2021-07-07 10:11:44 +0200

    Initial commit

---
+++ src/main/java/com/basiekjusz/mimuwcovidalert/MimuwCovidAlertApplication.java
@@ -0,0 +1,13 @@
+package com.basiekjusz.mimuwcovidalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimuwCovidAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimuwCovidAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basiekjusz/mimuwcovidalert/MimuwCovidAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basiekjusz.mimuwcovidalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimuwCovidAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
